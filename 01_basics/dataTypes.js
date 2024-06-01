89965eac21f8bcf64d5bdf007c0abcfb07c43b0e
shivamagrwal <kumarshivam2017@gmail.com>
2024-06-01 17:32:30 +0530

    added

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,5 @@
+let Name="Shivam"
+let age= 20
+let skills= null;
+
+console.table([typeof Name, typeof age, typeof skills, typeof undefined])
---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const phoneNo = 6205396792
+let Name = "Shivam Agrawal"
+var city = "Bokaro"
+state = "Jharkhand"
+let id;
+
+console.table([phoneNo , Name, city, state, id])
